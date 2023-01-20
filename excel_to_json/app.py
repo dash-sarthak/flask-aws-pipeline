2f8e52ca6fce1dd11e1dc82f2d8ea772e3336b5d
jayesh <jayeshn2000@gmail.com>
2023-01-20 23:32:54 +0530

    added GET and POST requests

---
+++ excel_to_json/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "flask-aws-pipeline under construction"
+
+#Post req
+@app.route("/convert",methods = ['POST'])
+def convert():
+    return "This is POST request"
+
+ #Get req
+@app.route("/pull",methods = ['GET'])
+def pull():
+    return "This is GET request"
+
+if __name__ == "main":
+    app.run()
